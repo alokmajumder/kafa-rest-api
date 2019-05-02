d25902138538175c7712e14b6dc57bc165c62ad9
alokmajumder <apu.sweden@gmail.com>
2019-05-02 15:46:48 +0200

    kafka rest

---
+++ src/main/java/com/ikea/kafka/springboot/kafkarestapi/KafkaRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ikea.kafka.springboot.kafkarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRestApiApplication.class, args);
+	}
+
+}
